a3d6de73bacd42eaf41be4bacf3c86bdf8fd0d01
Riya <ria020601@gmail.com>
2021-08-27 15:35:05 +0530

    new

---
+++ datatypes/datatypes.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("2*2=", 2*2)           //will print an interger value
+	fmt.Println("0.8/0.3 = ", 0.8/0.3) //will print a float value
+	fmt.Println("Hello, this will print a string.")
+	fmt.Printf("Is 32>43, %t", 32 > 43) //will print a bool value
+
+}

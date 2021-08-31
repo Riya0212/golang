2ac1fe5741a53d44eb1d9db0c8e42678444e30aa
Riya <ria020601@gmail.com>
2021-08-31 21:39:19 +0530

    integer added

---
+++ integer/integer.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a int
+	var b, c int8
+	a = 120
+	b = 24
+	c = 10
+	fmt.Printf("%v, %T", a, a)
+	fmt.Println("b+c= ", b+c)
+	fmt.Println("b+c= ", b-c)
+	fmt.Println("b+c= ", b*c)
+	fmt.Println("b+c= ", b/c)
+	fmt.Println("b+c= ", b%c)
+}
